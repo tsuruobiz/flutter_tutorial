2649d615ed2ced94ad04cdf33c861accbda521a7
tsuruooo <hideki.tsuruoka.fb@nttdocomo.com>
2023-11-28 15:44:51 +0900

    Step 2: Implement the title row

--- lib/main.dart
+++ lib/main.dart
@@ -9,30 +9,53 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(40),
-            color: Colors.yellow,
-            child: const Column(
-              children: [
-                Text('こんにちは世界!'),
-                Text('こんにちは世界!'),
-                Text('こんにちは世界!'),
-                Text('こんにちは世界!'),
-                Row(
-                  children: [
-                    Text('こんにちは世界!'),
-                    Icon(Icons.star, color: Colors.red),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        body: Column(
+          children: [
+            titleSection,
+          ],
         ),
       ),
     );
