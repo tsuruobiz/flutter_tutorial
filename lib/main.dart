3b86b3328492af35ca7b9c39dcf5c4efcc271cb9
tsuruooo <hideki.tsuruoka.fb@nttdocomo.com>
2023-11-28 15:51:36 +0900

    Step 3: Implement the button row

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,38 @@ class MainApp extends StatelessWidget {
       ),
     );
 
+    Column buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buildButtonColumn(color, Icons.call, 'CALL'),
+        buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -55,6 +87,7 @@ class MainApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
           ],
         ),
       ),
